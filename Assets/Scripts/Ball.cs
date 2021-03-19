c48063a5f6043d137cc31ef6039f2985cf21d411
Adrian Alcomendras <cloud102000@gmail.com>
2021-03-19 22:50:40 +0800

    Edit: Added basic animation controller with logic

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -9,6 +9,13 @@ public class Ball : MonoBehaviour
     private Rigidbody rigidbody;
     private bool thrown = false;
     public float horizontalSpeed;
+    public GameObject playerReference;
+    public float kickingDistance = 4.7f;
+    public float movementSpeed = 5f;
+    bool setForKick = false;
+    Vector3 lastPlayerPosition;
+    bool run = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +26,35 @@ public class Ball : MonoBehaviour
     void Update()
     {
         BallMovement();
+        Debug.Log(Vector3.Distance(transform.position, playerReference.transform.position));
+
+        if(Input.GetKeyDown(KeyCode.R)){
+            Animator playerAnimator = playerReference.GetComponent<Animator>();
+            run = !run;
+            playerAnimator.applyRootMotion = false;
+            playerAnimator.SetBool("Running", run);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            setForKick = true;
+            lastPlayerPosition = playerReference.transform.position;
+            Animator playerAnimator = playerReference.GetComponent<Animator>();
+
+            if(playerAnimator != null){
+                playerAnimator.applyRootMotion = false;
+                playerAnimator.SetBool("Running", false);
+                playerAnimator.applyRootMotion = true;
+                playerAnimator.SetTrigger("Kick");
+            }
+        }
+
+        if(setForKick){
+            if(Vector3.Distance(transform.position, lastPlayerPosition)<4.7){
+                 transform.position += transform.forward * Time.deltaTime * movementSpeed;
+            }else{
+                setForKick = false;
+            }
+        }
     }
 
     void BallMovement(){
