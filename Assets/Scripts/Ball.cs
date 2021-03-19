d1da53cafd77845f7786b3ab92049d98141b3411
Adrian Alcomendras <cloud102000@gmail.com>
2021-03-19 22:11:23 +0800

    Edit: Implemented basic bowling prototype

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [Range(1000,5000)]
+    public float ballForce;
+    private Rigidbody rigidbody;
+    private bool thrown = false;
+    public float horizontalSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BallMovement();
+    }
+
+    void BallMovement(){
+        if(!thrown){
+            float xMovement = Input.GetAxis("Horizontal");
+            Vector3 position = transform.position;
+            position.x += xMovement * horizontalSpeed;
+            transform.position = position;
+        }
+    }
+
+    public void PerformKick(){
+        Debug.Log("PerformKick");
+        if(!thrown){
+           thrown = true;
+           rigidbody.isKinematic = false;
+           //rigidbody.velocity = new Vector3(0,0,speed);
+           rigidbody.AddForce(transform.forward * ballForce);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerAnimatorHook.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimatorHook : MonoBehaviour
+{
+    public Ball ballReference;
+    
+    public void TriggerPerformKick(){
+        Debug.Log("TriggerPerformKick");
+        ballReference.PerformKick();
+    }
+}
